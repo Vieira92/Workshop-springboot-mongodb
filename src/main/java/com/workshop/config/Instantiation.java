de8b3ecd2152e467e842b30c6a8b98cd008f1742
Vieira92 <vieiraonwork@gmail.com>
2020-08-14 21:49:11 +0100

    Projection of author data with DTO

--- src/main/java/com/workshop/config/Instantiation.java
+++ src/main/java/com/workshop/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.workshop.dto.AuthorDTO;
 import com.workshop.entity.Post;
 import com.workshop.entity.User;
 import com.workshop.repository.PostRepository;
@@ -36,10 +37,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partir para viagem", "Vou viajar para Africa. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei feliz!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partir para viagem", "Vou viajar para Africa. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei feliz!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
