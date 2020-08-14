46136ecb51cd89ec984e5599d71bd948bbd7ff49
Vieira92 <vieiraonwork@gmail.com>
2020-08-14 22:39:47 +0100

    Adding comments to posts

--- src/main/java/com/workshop/config/Instantiation.java
+++ src/main/java/com/workshop/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.workshop.dto.AuthorDTO;
+import com.workshop.dto.CommentDTO;
 import com.workshop.entity.Post;
 import com.workshop.entity.User;
 import com.workshop.repository.PostRepository;
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partir para viagem", "Vou viajar para Africa. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei feliz!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/03/2020"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveita!", sdf.parse("22/03/2020"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tem um excelente dia!", sdf.parse("23/03/2020"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
