72c0c917b2a1d6d9d79d364fdc7bd72953a2189d
Vieira92 <vieiraonwork@gmail.com>
2020-08-14 22:20:13 +0100

    Getting a post by id

---
+++ src/main/java/com/workshop/service/PostService.java
@@ -0,0 +1,23 @@
+package com.workshop.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshop.entity.Post;
+import com.workshop.repository.PostRepository;
+import com.workshop.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
