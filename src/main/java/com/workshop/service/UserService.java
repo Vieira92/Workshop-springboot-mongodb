8cda2863d47ed76b599bca087f3ee9e923cf1b4a
Vieira92 <vieiraonwork@gmail.com>
2020-08-14 19:28:07 +0100

    Get a user by id

--- src/main/java/com/workshop/service/UserService.java
+++ src/main/java/com/workshop/service/UserService.java
@@ -1,22 +1,27 @@
 package com.workshop.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.entity.User;
 import com.workshop.repository.UserRepository;
+import com.workshop.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
 }
